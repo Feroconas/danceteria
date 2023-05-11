acdba03cd44c0042373674dcc120a82f36c87ebc
TiagoDomingosISCTE <tiagodomingos25@hotmail.com>
2023-05-11 18:30:31 +0100

    icon novo :3

--- webdanceteria/forms.py
+++ webdanceteria/forms.py
@@ -9,21 +9,24 @@ from .models import Membro
 class RegisterMemberForm(forms.ModelForm):
     class Meta:
         model = Membro
-        fields = ['user', 'nome', 'email', 'genero', 'descricao', 'data_nascimento', 'preferencias_musicais', 'imagem_perfil']
+        fields = ['nome', 'email', 'genero', 'descricao', 'data_nascimento', 'preferencias_musicais', 'imagem_perfil']
         labels = []
         widgets = {
-            'user.username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
-            'user.password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'type': 'Password'}),
             'nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'}),
             'genero': forms.RadioSelect(choices=Membro.genero_choices, attrs={'class': 'form-control'}),
             'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Descrição'}),
             'data_nascimento': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'preferencias_musicais': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Preferências Musicais'}),
-            'imagem_perfil': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'imagem_perfil': forms.ClearableFileInput(attrs={'class': 'form-control'})
         }
         label_suffix = ''
         attrs = {'class': 'register-form'}
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'type': 'password'}))
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
 
 # class InstrutorForm(forms.ModelForm):
 # especializacao = forms.ModelChoiceField(queryset=Categoria.objects.all())
