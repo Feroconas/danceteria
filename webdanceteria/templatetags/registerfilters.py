9bd0d0c0467fb620cdabc1c94c99b990bb43a75d
TiagoDomingosISCTE <tiagodomingos25@hotmail.com>
2023-05-14 02:17:25 +0100

    14/05 02h15

--- webdanceteria/templatetags/registerfilters.py
+++ webdanceteria/templatetags/registerfilters.py
@@ -16,9 +16,6 @@ def isMembro(user):
 def isInstrutor(user):
     return user.groups.filter(name='Instrutores').exists()
 
-@register.filter
-def isSuperUser(user):
-    return user.is_superuser
 
 @register.filter
 def getUtilizador(user):
